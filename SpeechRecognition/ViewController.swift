8d68fb0254813aac74bacd6a935a8e97bcc14521
Evgen Kolenkov <kolenkoff@icloud.com>
2018-09-19 15:06:01 +0300

    Initial Commit

---
+++ SpeechRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognition
+//
+//  Created by Evgeniy Kolenkov on 9/19/18.
+//  Copyright © 2018 Evgeniy Kolenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
